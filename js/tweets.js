3c659fb6ca0cb195a69f55ff29f13bac3918ad61
Albin Martinsson <albinmartinsson91@gmail.com>
2016-03-31 14:22:55 +0200

    Nu pratas det.

--- js/tweets.js
+++ js/tweets.js
@@ -10,9 +10,16 @@ $(function(){
 				var $tweets = $('<ul></ul>');
 				$.each(response, function(i, obj) {
 					$tweets.append('<li>' + obj.text + '</li>');
+					console.log(obj.text);
+					responsiveVoice.speak(obj.text);
 				});
 
 				$('.tweets-container').html($tweets);
+
+				responsiveVoice.OnVoiceReady = function() {
+					  console.log(obj.text);
+					  responsiveVoice.speak($tweets);
+					};
 
 			} else {
 				$('.tweets-container p:first').text('Response error');
